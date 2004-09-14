989d15bb484e421a36353d63dab474d8fa4a95be
kevins <kevins@localhost>
2004-09-14 23:51:13 +0000

    script to create tunnels for running a server on linux without being root

---
+++ source/org/martus/server/main/create-tunnels.bash
@@ -0,0 +1,4 @@
+ssh -f -N -L 988:localhost:9988 root@localhost
+ssh -f -N -L 987:localhost:9987 root@localhost
+ssh -f -N -L 443:localhost:9443 root@localhost
+ssh -f -N -L  80:localhost:9080 root@localhost
