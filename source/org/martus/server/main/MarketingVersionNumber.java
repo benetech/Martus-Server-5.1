6d161a6c4d5de9239ad5eb91c8b85243a704d2f0
charles <charles@localhost>
2005-03-28 20:51:26 +0000

    new internal version #

--- source/org/martus/server/main/MarketingVersionNumber.java
+++ source/org/martus/server/main/MarketingVersionNumber.java
@@ -28,5 +28,5 @@ package org.martus.server.main;
 
 public class MarketingVersionNumber
 {
-	public static String marketingVersionNumber = "2.3.0";
+	public static String marketingVersionNumber = "2.4.0 Internal";
 }
