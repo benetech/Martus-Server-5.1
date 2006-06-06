41e973f24f5684ee0ab316b70e80d09de52e0f63
charles <charles@localhost>
2006-06-06 22:37:24 +0000

    mTime checking must be padded for Java's FastTime, build failed on build machine

--- source/org/martus/server/formirroring/TestMirroringRetriever.java
+++ source/org/martus/server/formirroring/TestMirroringRetriever.java
@@ -487,7 +487,7 @@ public class TestMirroringRetriever extends TestCaseEnhanced
 		Bulletin b1 = new Bulletin(security);
 		b1.setSealed();
 		store.saveBulletinForTesting(b1);
-		long fastTimeVarianceMS = 2000; //2 seconds
+		long fastTimeVarianceMS = 3000; //3 seconds
 		Thread.sleep(2*fastTimeVarianceMS);//Ensure that the mTimes will be different between saving to the database and creating the zip file.
 
 		DatabaseKey key = b1.getDatabaseKey();
@@ -500,8 +500,8 @@ public class TestMirroringRetriever extends TestCaseEnhanced
 		zip.close();
 
 		long entryTime = entry.getTime();
-		long difference = (mTimeOriginal-entryTime);
-		assertTrue("Zip file created before mTime of bulletin?", difference > 0 );
+		long difference = (mTimeOriginal-(entryTime));
+		assertTrue("Zip file created before mTime of bulletin?", (difference + fastTimeVarianceMS) > 0);
 		assertTrue("Zip file doesn't have the real mTime of the bulletin?", difference < fastTimeVarianceMS);
 
 		store.deleteAllBulletins();
