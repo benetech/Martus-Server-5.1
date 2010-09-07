0f2b193511befc6796feb474ea85d925908408c3
kevins <kevins@localhost>
2010-09-07 17:10:04 +0000

    Refactor rename two methods

--- source/org/martus/server/tools/CreateStatistics.java
+++ source/org/martus/server/tools/CreateStatistics.java
@@ -613,7 +613,7 @@ public class CreateStatistics
 					String fieldDataPacketId = bhp.getFieldDataPacketId();
 					DatabaseKey fieldKey = DatabaseKey.createSealedKey(UniversalId.createFromAccountAndLocalId(
 						bhp.getAccountId(), fieldDataPacketId));
-					FieldSpec[] standardPublicFieldSpecs = StandardFieldSpecs.getDefaultTopSectionFieldSpecs();
+					FieldSpec[] standardPublicFieldSpecs = StandardFieldSpecs.getDefaultTopSectionFieldSpecsAsArray();
 					FieldDataPacket fdp = new FieldDataPacket(UniversalId.createFromAccountAndLocalId(
 						bhp.getAccountId(), fieldDataPacketId), standardPublicFieldSpecs);
 					FileInputStreamWithSeek in = new FileInputStreamWithSeek(fileDatabase.getFileForRecord(fieldKey));
