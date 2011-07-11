44043e7b1c01509319d857147e4e1ce6b20e11cd
kevins <kevins@localhost>
2011-07-11 14:17:36 +0000

    Add helpful output to create-tunnels bash script

--- source/org/martus/server/main/create-tunnels.bash
+++ source/org/martus/server/main/create-tunnels.bash
@@ -19,10 +19,17 @@
 #   sh create-tunnels.bash
 #   exit
 
+echo "creating tunnel 988->9988"
 sudo ssh -f -N -L 988:localhost:9988 $USER@localhost
+echo "creating tunnel 987->9987"
 sudo ssh -f -N -L 987:localhost:9987 $USER@localhost
+echo "creating tunnel 986->9986"
 sudo ssh -f -N -L 986:localhost:9986 $USER@localhost
+echo "creating tunnel 985->9985"
 sudo ssh -f -N -L 985:localhost:9985 $USER@localhost
+echo "creating tunnel 984->9984"
 sudo ssh -f -N -L 984:localhost:9984 $USER@localhost
+echo "creating tunnel 443->9443"
 sudo ssh -f -N -L 443:localhost:9443 $USER@localhost
+echo "creating tunnel 80->9080"
 sudo ssh -f -N -L  80:localhost:9080 $USER@localhost
