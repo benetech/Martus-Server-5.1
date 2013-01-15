d9d778c3b538b67900cc1177e4676758028929f0
Kevin Smith <Kevin Smith@localhost>
2013-01-15 14:08:31 -0500

    Bump server version number to 4.4

--- source/org/martus/server/main/MarketingVersionNumber.java
+++ source/org/martus/server/main/MarketingVersionNumber.java
@@ -28,5 +28,5 @@ package org.martus.server.main;
 
 public class MarketingVersionNumber
 {
-	public static String marketingVersionNumber = "4.2";
+	public static String marketingVersionNumber = "4.4";
 }
