fe21d3d9ca48361133ca4cedf176b6b31ce9b3d8
Kevin Smith <Kevin Smith@localhost>
2014-04-17 16:07:37 -0700

    Manually extract MirrorPuller test into its own test class

--- source/org/martus/server/formirroring/TestServerForMirroring.java
+++ source/org/martus/server/formirroring/TestServerForMirroring.java
@@ -120,29 +120,6 @@ public class TestServerForMirroring extends TestCaseEnhanced
 		
 	}
 	
-	public void testLoadMirrorsToCall() throws Exception
-	{
-		MockMartusServer noCallsToMakeCore = new MockMartusServer();
-		MirrorPuller noCallsToMake = new MirrorPuller(noCallsToMakeCore, logger);
-		noCallsToMake.createGatewaysWeWillCall();
-		assertEquals(0, noCallsToMake.retrieversWeWillCall.size());
-		noCallsToMakeCore.deleteAllFiles();
-		
-		MockMartusServer twoCallsToMakeCore = new MockMartusServer();
-		twoCallsToMakeCore.enterSecureMode();
-		File mirrorsWhoWeCall = new File(twoCallsToMakeCore.getStartupConfigDirectory(), "mirrorsWhoWeCall");
-		mirrorsWhoWeCall.mkdirs();
-		File pubKeyFile1 = new File(mirrorsWhoWeCall, "code=1.2.3.4.5-ip=1.2.3.4.txt");
-		MartusUtilities.exportServerPublicKey(clientSecurity1, pubKeyFile1);
-		File pubKeyFile2 = new File(mirrorsWhoWeCall, "code=2.3.4.5.6-ip=2.3.4.5.txt");
-		MartusUtilities.exportServerPublicKey(clientSecurity2, pubKeyFile2);
-		MirrorPuller twoCallsToMake = new MirrorPuller(twoCallsToMakeCore, logger);
-		twoCallsToMake.createGatewaysWeWillCall();
-		assertEquals(2, twoCallsToMake.retrieversWeWillCall.size());
-		mirrorsWhoWeCall.delete();
-		twoCallsToMakeCore.deleteAllFiles();
-	}
-	
 	public void testIsAuthorizedForMirroring() throws Exception
 	{
 		MockMartusServer nobodyAuthorizedCore = new MockMartusServer();
